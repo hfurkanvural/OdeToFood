5a1ca948caf9c6c913babeefa8691a83ad9bf73e
hfurkanvural <hfurkanvural@gmail.com>
2020-04-25 23:10:09 +0300

    db connection and EF added.

--- OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood.Data/IRestaurantData.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using OdeToFood.Core;
 
 namespace OdeToFood.Data
@@ -9,32 +7,10 @@ namespace OdeToFood.Data
     {
         IEnumerable<Restaurant> GetRestaurantsByName(string name);
         Restaurant GetById(int id);
+        Restaurant Update(Restaurant updatedRestaurant);
+        Restaurant Add(Restaurant newRestaurant);
+        Restaurant Delete(int id);
+        int Commit();
 
-    }
-
-    public class InMemoryRestaurantData : IRestaurantData
-    {
-        List<Restaurant> restaurants;
-
-        public InMemoryRestaurantData()
-        {
-            restaurants = new List<Restaurant>()
-            {
-                new Restaurant { Id = 1, Name = "Scott's Pizza", Location = "Maryland", Cuisine = CuisineType.Italian},
-                new Restaurant { Id = 2, Name = "Cinnamon Club", Location = "London", Cuisine = CuisineType.None},
-                new Restaurant { Id = 3, Name = "La Costa", Location = "California", Cuisine = CuisineType.Mexican}
-            };
-        }
-        public Restaurant GetById(int id)
-        {
-            return restaurants.SingleOrDefault(r => r.Id == id);
-        }
-        public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
-        {
-            return  from r in restaurants
-                    where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
-                    orderby r.Name
-                    select r;
-        }
     }
 }
---
+++ OdeToFood.Data/OdeToFoodDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using OdeToFood.Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace OdeToFood.Data
+{
+
+    public class OdeToFoodDbContext : DbContext
+    {
+        public OdeToFoodDbContext(DbContextOptions<OdeToFoodDbContext> options)
+            :base(options)
+        {
+            
+        }
+        public DbSet<Restaurant> Restaurants { get; set; }
+    
+    }
+}
